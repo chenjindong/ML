60cb251aebf4142f17f3d6290a65390bf44a445f
chenjindong <892940801@qq.com>
2016-09-27 10:54:25 +0800

    add pandas dummy
    add pandas dummy

--- Pandas/DataFrameTest.py
+++ Pandas/DataFrameTest.py
@@ -7,7 +7,6 @@ data = {'state': ['Ohio', 'Ohio', 'Ohio', 'Nevada', 'Nevada'],
         'year': [2000, 2001, 2002, 2001, 2002],
         'pop': [1.5, 1.7, 3.6, 2.4, 2.9]}
 df = pd.DataFrame(data,  columns=['state', 'year', 'pop', 'debt'])  # you can specify the order of column by parameter columns, and the index by parameter index
-
 # print(df)
 # print(df['state'])  # print the column of state
 # print(df.state)
@@ -21,19 +20,20 @@ df = pd.DataFrame(data,  columns=['state', 'year', 'pop', 'debt'])  # you can sp
 # del df['eastern']  # delete a column
 # print(df)
 
-print('state' in df.columns)
-print(2 in df.index)
-print(5 in df.index)
+# print('state' in df.columns)
+# print(2 in df.index)
+# print(5 in df.index)
 
+# dummy non-numerical value
+obj1 = pd.DataFrame({'Month': ['monday', 'tuesday', 'wednesday']})
+# print(obj1.describe)
+month = pd.get_dummies(obj1['Month'])
+# print(month.columns.values)
+# print(month.values)
+# obj1[month.columns.values] = month.values
+for i in [0, 1, 2]:
+        obj1[month.columns.values[i]] = month.values[i]
 
-
-
-
-
-
-
-
-
-
-
+print(obj1.drop(['Month'], axis=1))
+print(obj1)
 
