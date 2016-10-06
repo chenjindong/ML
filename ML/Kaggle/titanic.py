4a7ca4386dad23b0c588f764533e04962c69f679
chenjindong <892940801@qq.com>
2016-10-06 10:11:07 +0800

    merge titanic01 and titanic

--- ML/Kaggle/titanic.py
+++ ML/Kaggle/titanic.py
@@ -1,64 +1,54 @@
-# [ 0.77653631  0.7877095   0.78089888  0.76404494  0.81920904] Logistic Regression
-# [ 0.83798883  0.82122905  0.80898876  0.79775281  0.85875706] SVM with RBF
-# [ 0.83798883  0.82122905  0.80898876  0.79775281  0.85875706] SVM with linear Kernal
-# [ 0.79329609  0.79329609  0.86516854  0.78089888  0.84180791] Random Forest
-from sklearn import linear_model
 import pandas as pd
-import numpy as np
+from sklearn import linear_model
 from sklearn import svm
-from matplotlib import pyplot as plt
+from sklearn import ensemble
 from sklearn import cross_validation
 from sklearn import preprocessing
-from sklearn import ensemble
 
-# data pre-processing
 data = pd.read_csv("C:\\Users\\cjd\\Desktop\\titanic\\train.csv")
-trainY = data['Survived']
-trainX = data.copy()
-del trainX['PassengerId'], trainX['Name'], trainX['Survived'], trainX['Ticket'], trainX['Cabin']
-trainX['Sex'] = (trainX['Sex'] == 'male')
-trainX = trainX.fillna(trainX.mean())  # 填充空值的方法
-embarked = pd.get_dummies(trainX['Embarked'])  # dummy variable
-del trainX['Embarked']
-for i in np.arange(3):
-    trainX[embarked.columns.values[i]] = embarked[embarked.columns.values[i]]
-scaler = preprocessing.StandardScaler().fit(trainX)  # 数据归一化
+trainX = data.drop(['Name', 'Ticket', 'Cabin', 'Survived', 'PassengerId'], axis=1)
+trainY = data['Survived'].copy()  # 如果不加copy，则是引用
+# 数据预处理
+# 空值填充
+trainX['Age'] = trainX['Age'].fillna(-1)
+trainX['Embarked'] = trainX['Embarked'].fillna('S')
+# dummy化非数值型变量
+embarked = pd.get_dummies(trainX['Embarked'])
+trainX = trainX.drop(['Embarked'], axis=1).join(embarked)  # drop a column and add columns to dataframe
+sex = pd.get_dummies(trainX['Sex'])
+trainX = trainX.drop(['Sex'], axis=1).join(sex)
+# 数据标准化
+scaler = preprocessing.StandardScaler().fit(trainX)
 trainX = scaler.transform(trainX)
 
-
-# model selection
-# Logistic Regression
-# lr = linear_model.LogisticRegression()
-# lr.fit(trainX, trainY)
-# scores = cross_validation.cross_val_score(lr, trainX, trainY, cv=5)
-# print(scores)
-# # SVM
-# svc = svm.SVC()
-# svc.fit(trainX, trainY)
-# scores = cross_validation.cross_val_score(svc, trainX, trainY, cv=5)
-# print(scores)
-# Random Forest
-rf = ensemble.RandomForestClassifier(n_estimators=50, max_features=8)
+# 模型选择
+# logistic regression
+lr = linear_model.LogisticRegression()
+lr.fit(trainX, trainY)
+scores = cross_validation.cross_val_score(lr, trainX, trainY, cv=5)
+print(scores.mean())
+# SVM
+svc = svm.SVC()
+svc.fit(trainX, trainY)
+scores = cross_validation.cross_val_score(svc, trainX, trainY, cv=5)
+print(scores.mean())
+# random forest
+rf = ensemble.RandomForestClassifier(n_estimators=100)
 rf.fit(trainX, trainY)
-print(rf)
 scores = cross_validation.cross_val_score(rf, trainX, trainY, cv=5)
-print(scores)
+print(scores.mean())
 
-
-# predict
+# 预测
 data = pd.read_csv("C:\\Users\\cjd\\Desktop\\titanic\\test.csv")
-testX = data.copy()
-del testX['PassengerId'], testX['Name'], testX['Ticket'], testX['Cabin']
-testX['Sex'] = (testX['Sex'] == 'male')
-testX = testX.fillna(testX.mean())  # 填充空值的方法
-embarked = pd.get_dummies(testX['Embarked'])  # dummy variable
-del testX['Embarked']
-for i in np.arange(3):
-    testX[embarked.columns.values[i]] = embarked[embarked.columns.values[i]]
+testX = data.drop(['Name', 'Cabin', 'Ticket', 'PassengerId'], axis=1)
+testX['Age'] = testX['Age'].fillna(-1)
+testX['Fare'] = testX['Fare'].fillna(testX['Fare'].mean())
+embarked = pd.get_dummies(testX['Embarked'])
+testX = testX.drop(['Embarked'], axis=1).join(embarked)  # drop a column and add columns to dataframe
+sex = pd.get_dummies(testX['Sex'])
+testX = testX.drop(['Sex'], axis=1).join(sex)
 testX = scaler.transform(testX)
 
-Y = rf.predict(testX)
-result = pd.DataFrame({'Survived': Y}, columns={'PassengerId', 'Survived'})
-result['PassengerId'] = data['PassengerId']
+testY = rf.predict(testX)
+result = pd.DataFrame({'PassengerId': data['PassengerId'], 'Survived': testY})
 result.to_csv("C:\\Users\\cjd\\Desktop\\titanic\\output.csv")
-# print(result)
--- Pandas/DataFrameTest.py
+++ Pandas/DataFrameTest.py
@@ -6,34 +6,6 @@ import pandas as pd
 data = {'state': ['Ohio', 'Ohio', 'Ohio', 'Nevada', 'Nevada'],
         'year': [2000, 2001, 2002, 2001, 2002],
         'pop': [1.5, 1.7, 3.6, 2.4, 2.9]}
-df = pd.DataFrame(data,  columns=['state', 'year', 'pop', 'debt'])  # you can specify the order of column by parameter columns, and the index by parameter index
-# print(df)
-# print(df['state'])  # print the column of state
-# print(df.state)
-# print(df.ix[2])  # print the row of 2
-
-# df['debt'] = np.arange(5)  # assign value for column debt
-# print(df)
-
-# df['eastern'] = df['state'] == 'Ohio'  # add a new column
-# print(df)
-# del df['eastern']  # delete a column
-# print(df)
-
-# print('state' in df.columns)
-# print(2 in df.index)
-# print(5 in df.index)
-
-# dummy non-numerical value
-obj1 = pd.DataFrame({'Month': ['monday', 'tuesday', 'wednesday']})
-# print(obj1.describe)
-month = pd.get_dummies(obj1['Month'])
-# print(month.columns.values)
-# print(month.values)
-# obj1[month.columns.values] = month.values
-for i in [0, 1, 2]:
-        obj1[month.columns.values[i]] = month.values[i]
-
-print(obj1.drop(['Month'], axis=1))
-print(obj1)
+df = pd.DataFrame(data,  columns=['state', 'year', 'pop', 'debt'])
+print(df.info())  # get the data type and non-null data number
 
