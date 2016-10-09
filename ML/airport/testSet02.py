4571fd202e7f9328e7c9d7ac18e225c0471a5d48
chenjindong <892940801@qq.com>
2016-10-09 14:32:54 +0800

    average of 12,13 to predict 14

--- ML/airport/testSet02.py
+++ ML/airport/testSet02.py
@@ -8,13 +8,28 @@ for hour in [15, 16, 17]:
 # df.to_csv('C:\\Users\\cjd\\Desktop\\airport\\testSet\\time.csv')
 
 wifi_ap = pd.read_csv('C:\\Users\\cjd\\Desktop\\airport\\WIFI_AP_Passenger_Records_chusai_1stround.csv')
+result01 = wifi_ap[(wifi_ap['timeStamp'] > '2016-09-12-15') & (wifi_ap['timeStamp'] < '2016-09-12-18')]
+result01 = result01.sort_values(['WIFIAPTag', 'timeStamp'])
+result02 = wifi_ap[(wifi_ap['timeStamp'] > '2016-09-13-15') & (wifi_ap['timeStamp'] < '2016-09-13-18')]
+result02 = result02.sort_values(['WIFIAPTag', 'timeStamp'])
+
+# result.to_csv('C:\\Users\\cjd\\Desktop\\airport\\wifi_ap_preprocess\\data01.csv')
+
+passengerCount01 = result01.passengerCount.tolist()
+passengerCount02 = result02.passengerCount.tolist()
 WIFIAPTag = wifi_ap.WIFIAPTag.unique().tolist()
+
 data = []
+idx = 0
 for col1 in WIFIAPTag:
     for col2 in time:
-        data.append([15, col1, col2])
+        temp = 0
+        for i in range(10):
+            temp = temp + passengerCount01[idx] + passengerCount02[idx]
+            idx += 1
+        data.append([temp/20.0, col1, col2])
 df = pd.DataFrame(data, columns=['passengerCount', 'WIFIAPTag', 'slice10min'])
-df.to_csv('C:\\Users\\cjd\\Desktop\\airport\\testSet\\airport_gz_passenger_predict.csv')
+df.to_csv('C:\\Users\\cjd\\Desktop\\airport\\testSet\\airport_gz_passenger_predict02.csv')
 
 
 
--- ML/airport/wifi_ap.py
+++ ML/airport/wifi_ap.py
@@ -4,28 +4,29 @@ import matplotlib.pyplot as plt
 
 
 wifi_ap = pd.read_csv('C:\\Users\\cjd\\Desktop\\airport\\WIFI_AP_Passenger_Records_chusai_1stround.csv')
-tags = wifi_ap[(wifi_ap.WIFIAPTag < 'E3') & (wifi_ap.WIFIAPTag > 'E2')].WIFIAPTag.unique()
-print(tags)
+# tags = wifi_ap[(wifi_ap.WIFIAPTag < 'E3') & (wifi_ap.WIFIAPTag > 'E2')].WIFIAPTag.unique()
+# print(tags)
 
 
 # print(wifi_ap.info())
 # print(wifi_ap.tail())
 
-# result = wifi_ap[wifi_ap['WIFIAPTag'] == 'E1-2A<E1-2A-06>']
-# result.to_csv('C:\\Users\\cjd\\Desktop\\airport\\wifi_ap_preprocess\\E1-2A(E1-2A-06).csv')
+# result = wifi_ap[(wifi_ap['timeStamp'] > '2016-09-13-15') & (wifi_ap['timeStamp'] < '2016-09-13-18')]
+# result.to_csv('C:\\Users\\cjd\\Desktop\\airport\\wifi_ap_preprocess\\data01.csv')
 
-x = np.arange(0, 4000, 1)
-left = 0
-right = 4000
 
-y = wifi_ap[wifi_ap.WIFIAPTag == 'E1-1A-1<E1-1-01>'].passengerCount[left:right]
-plt.plot(x, y, color='red')
-
-y = wifi_ap[wifi_ap.WIFIAPTag == 'E2-3B<E2-3-20>'].passengerCount[left:right]
-plt.plot(x, y, color='green')
-
-y = wifi_ap[wifi_ap.WIFIAPTag == 'E2-3B<E2-3-21>'].passengerCount[left:right]
-plt.plot(x, y, color='blue')
-plt.grid()
-plt.show()
+# x = np.arange(0, 4000, 1)
+# left = 0
+# right = 4000
+#
+# y = wifi_ap[wifi_ap.WIFIAPTag == 'E1-1A-1<E1-1-01>'].passengerCount[left:right]
+# plt.plot(x, y, color='red')
+#
+# y = wifi_ap[wifi_ap.WIFIAPTag == 'E2-3B<E2-3-20>'].passengerCount[left:right]
+# plt.plot(x, y, color='green')
+#
+# y = wifi_ap[wifi_ap.WIFIAPTag == 'E2-3B<E2-3-21>'].passengerCount[left:right]
+# plt.plot(x, y, color='blue')
+# plt.grid()
+# plt.show()
 
