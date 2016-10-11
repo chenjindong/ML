2fe72747e3678462778bcdde96ede50f5a27eb94
cjd <892940801@qq.com>
2016-10-11 18:29:12 +0800

    change security_check

--- ML/airport/security_check.py
+++ ML/airport/security_check.py
@@ -5,6 +5,14 @@ data = pd.read_csv('C:\\Users\\cjd\\Desktop\\airport\\airport_gz_security_check_
 
 result = data.drop(['passenger_ID'], axis=1)
 
-result = result.groupby(['flight_ID'])
+grouped_result = result.groupby(['flight_ID']).count()
+flight = grouped_result[grouped_result.security_time > 20].index.tolist()
+# print(flight)
+result = result[result.flight_ID .isin(flight)]
+grouped_result = result.groupby(['flight_ID'])
+for name, group in grouped_result:
+    if name == 'ZH9764':
+        print(group)
+
 # result.to_csv('C:\\Users\\cjd\\Desktop\\airport\\security_check\\result.csv')
-print(result.count())
+# print(result.count())
