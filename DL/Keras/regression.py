e903d55b70cdd6783f63980ef705527c35c25181
slmady <892940801@qq.com>
2017-11-24 10:26:23 +0800

    replace keras/regression.py

--- DL/Keras/regression.py
+++ DL/Keras/regression.py
@@ -1,52 +1,28 @@
+#!/home/user0/anaconda3/bin/python
+import keras
+from keras.models import Model
+from keras.layers import Input, Dense
 import numpy as np
-np.random.seed(1337)
-import matplotlib.pyplot as plt
-from keras.models import Sequential
-from keras.layers import Dense      # 全连接层
 
-x = np.linspace(-1, 1, 200)
+# train dataset and test dataset
+x = np.arange(10000)/10000  # data normalization before training
 np.random.shuffle(x)
-y = 0.5*x + 2 + np.random.normal(0, 0.05, (200,))
+y = 2*x + 3 + np.random.normal(0, 0.05, (10000))  # add noise
+x_train, y_train = x[:7000], y[:7000]
+x_test, y_test = x[7000:], y[7000:]
 
-#plt.scatter(x, y)
-#plt.show()
+# model
+a = Input(shape=(1,))
+b = Dense(1)(a)
+model = Model(input=a, output=b)
+model.compile(optimizer='sgd', loss='mse')
+model.summary()
+model.fit(x_train, y_train, batch_size=32, epochs=10, verbose=1, validation_data=(x_test, y_test))
 
-x_train, y_train = x[:160], y[:160]
-x_test, y_test = x[160:], y[160:]
-
-model = Sequential()
-model.add(Dense(output_dim=1, input_dim=1))
-model.compile(loss='mse', optimizer='sgd')
-
-# train
-for step in range(300):
-    cost = model.train_on_batch(x_train, y_train)
-    if step%100 == 0:
-        print('train cost ', cost)
-
-# test
-cost = model.evaluate(x_test, y_test, batch_size=40)
-print('test cost ', cost)
-W, b = model.layers[0].get_weights()
-print('weights=', W, '\nbiases=', b)
-y_predict = model.predict(x_test)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+layer = model.get_layer('dense_1')
+print('layer.get_weights: ', layer.get_weights())
+print('layer.input: ', layer.input)
+print('layper.output: ', layer.output)
+print('layer.input_shape: ', layer.input_shape)
+print('layer.output_shape: ', layer.output_shape)
 
