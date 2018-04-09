ddd95b00df83920e15279f012a74659e6cad4ad6
chenjindong <892940801@qq.com>
2018-04-09 10:46:34 +0800

    update skip-gram

--- DL/Keras/embedding/skip_gram.py
+++ DL/Keras/embedding/skip_gram.py
@@ -1,62 +1,81 @@
-#!/home/user0/anaconda3/bin/python
+import cjdpy
+import time
 from keras.models import Model
-from keras.layers.embeddings import Embedding
-from keras.engine.topology import Input
-from keras.layers.merge import Dot
-from keras.layers.core import Reshape, Dense
+from keras.layers import *
 from keras.preprocessing.sequence import skipgrams
-import numpy as np
+from keras.callbacks import ModelCheckpoint
+import tensorflow as tf
+from keras.backend.tensorflow_backend import set_session
+
 
 '''
-Using keras to implement skip-gram algorithm
+skip-gram algorithm using negative sampling
 '''
 
+start = time.clock()
+
+config = tf.ConfigProto()
+config.gpu_options.per_process_gpu_memory_fraction = 0.4
+set_session(tf.Session(config=config))
+
+# parameters
 embedding_dims = 300
 batch_size = 32
-epochs = 5
-path = r'\\10.141.208.22\data\Chinese_isA\corpus\wikicorpus_seg.txt'
+epochs = 20
+path_windows = r'\\10.141.208.22\data\Chinese_isA\corpus\wikicorpus_seg.txt'
+path_linux = 'wikicorpus_seg.txt'
 
 # dataset
-texts = []
-vocabulary = []
-with open(path, 'r', encoding='utf-8') as fin:
-    for i, line in enumerate(fin):
-        if i > 10000:
-            break
-        tokens = line.strip().split('\t')
-        vocabulary.extend(tokens)
-        texts.append(tokens)
+texts, vocabulary = [], []
+
+data = cjdpy.load_csv(path_windows)
+# data = cjdpy.load_csv(path_linux)
+
+for i, item in enumerate(data):
+    if i > 10000: break
+    vocabulary += list(item)
+    texts.append(item)
+
 vocabulary = set(vocabulary)
 vocab_size = len(vocabulary)
 word2id, id2word = {}, {}
 for idx, word in enumerate(vocabulary):
     word2id[word] = idx
     id2word[idx] = word
-# text_seq = []
-# for text in texts:
-#     text_seq.append([word2id[word] for word in text])
+
 text_seq = [word2id[word] for text in texts for word in text]
-pairs, labels = skipgrams(text_seq, len(word2id), window_size=4)
-x_train, y_train = pairs[:-10000], labels[:-10000]
-x_test, y_test = pairs[-10000:], labels[-10000:]
-x_train, y_train = np.array(x_train), np.array(y_train)
-x_test, y_test = np.array(x_test), np.array(y_test)
 
-# model
-input1 = Input(shape=(1,))
-embeddings1 = Embedding(vocab_size, embedding_dims)(input1)
-output1 = Reshape(target_shape=(embedding_dims,))(embeddings1)
+X_train, y_train = skipgrams(text_seq, len(word2id), window_size=4)
+X_train, y_train = np.array(X_train), np.array(y_train)
 
-input2 = Input(shape=(1,))
-embeddings2 = Embedding(vocab_size, embedding_dims)(input2)
-output2 = Reshape(target_shape=(embedding_dims,))(embeddings2)
-
-dot = Dot(axes=-1)([output1, output2])
-output = Dense(units=1, activation='sigmoid')(dot)
-model = Model(inputs=(input1, input2), outputs=output)
-model.compile(optimizer='adam', loss='mse')
-model.summary()
-
-model.fit(x=[x_train[:, 0], x_train[:, 1]], y=y_train, batch_size=batch_size, epochs=epochs, verbose=1)
+print('show 5 training examples...')
+for i in range(5):
+    print(X_train[i], y_train[i])
 
 
+def build_skipgram_model():
+    input1 = Input(shape=(1,))
+    embeddings1 = Embedding(vocab_size, embedding_dims)(input1)
+    output1 = Flatten()(embeddings1)
+
+    input2 = Input(shape=(1,))
+    embeddings2 = Embedding(vocab_size, embedding_dims)(input2)
+    output2 = Flatten()(embeddings2)
+
+    dot = Dot(axes=-1)([output1, output2])
+    output = Dense(units=1, activation='sigmoid')(dot)
+    model = Model(inputs=(input1, input2), outputs=output)
+    return model
+
+
+m = build_skipgram_model()
+m.compile(optimizer='adam', loss='binary_crossentropy')
+m.summary()
+
+modelCheckpoint = ModelCheckpoint('skip_gram_model.h5', save_best_only=True, save_weights_only=True)
+
+m.fit(x=[X_train[:, 0], X_train[:, 1]], y=y_train, batch_size=batch_size, validation_split=0.2,
+          epochs=epochs, verbose=1, callbacks=[modelCheckpoint])
+
+
+print(time.clock()-start)
