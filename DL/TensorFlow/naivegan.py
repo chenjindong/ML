6132d68080c16b0c86375ca35d291b94cb8d9b5c
陈金栋 <892940801@qq.com>
2018-08-15 10:15:56 +0800

    naivegan

--- DL/TensorFlow/naivegan.py
+++ DL/TensorFlow/naivegan.py
@@ -1,5 +1,3 @@
-#!/home/gdmlab/anaconda3/bin/python
-#coding:utf-8
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 import numpy as np
@@ -15,7 +13,7 @@ to_train = True
 to_restore = False
 output_path = "output"
 
-max_epoch = 500
+max_epoch = 100
 
 h1_size = 150
 h2_size = 300
@@ -29,19 +27,11 @@ def build_generator(z_prior):
     x_generate: (batch_size, 784)
     function G: z => image
     '''
-    w1 = tf.Variable(tf.truncated_normal([z_size, h1_size], stddev=0.1), name="g_w1", dtype=tf.float32)
-    b1 = tf.Variable(tf.zeros([h1_size]), name="g_b1", dtype=tf.float32)
-    h1 = tf.nn.relu(tf.matmul(z_prior, w1) + b1)
-    w2 = tf.Variable(tf.truncated_normal([h1_size, h2_size], stddev=0.1), name="g_w2", dtype=tf.float32)
-    b2 = tf.Variable(tf.zeros([h2_size]), name="g_b2", dtype=tf.float32)
-    h2 = tf.nn.relu(tf.matmul(h1, w2) + b2)
-    w3 = tf.Variable(tf.truncated_normal([h2_size, img_size], stddev=0.1), name="g_w3", dtype=tf.float32)
-    b3 = tf.Variable(tf.zeros([img_size]), name="g_b3", dtype=tf.float32)
-    h3 = tf.matmul(h2, w3) + b3
-    x_generate = tf.nn.tanh(h3)
-    g_params = [w1, b1, w2, b2, w3, b3]
-    return x_generate, g_params
-
+    with tf.variable_scope('generator'):
+        h1 = tf.layers.dense(z_prior, h1_size, activation=tf.nn.relu)
+        h2 = tf.layers.dense(h1, h2_size, activation=tf.nn.relu)
+        h3 = tf.layers.dense(h2, img_size, activation=tf.nn.tanh)
+        return h3
 
 def build_discriminator(x_data, x_generated, keep_prob):
     '''
@@ -50,20 +40,21 @@ def build_discriminator(x_data, x_generated, keep_prob):
     function D: x_data => y_data
                 x_generated => y_generated
     '''
-    x_in = tf.concat([x_data, x_generated], 0)
-    w1 = tf.Variable(tf.truncated_normal([img_size, h2_size], stddev=0.1), name="d_w1", dtype=tf.float32)
-    b1 = tf.Variable(tf.zeros([h2_size]), name="d_b1", dtype=tf.float32)
-    h1 = tf.nn.dropout(tf.nn.relu(tf.matmul(x_in, w1) + b1), keep_prob)
-    w2 = tf.Variable(tf.truncated_normal([h2_size, h1_size], stddev=0.1), name="d_w2", dtype=tf.float32)
-    b2 = tf.Variable(tf.zeros([h1_size]), name="d_b2", dtype=tf.float32)
-    h2 = tf.nn.dropout(tf.nn.relu(tf.matmul(h1, w2) + b2), keep_prob)
-    w3 = tf.Variable(tf.truncated_normal([h1_size, 1], stddev=0.1), name="d_w3", dtype=tf.float32)
-    b3 = tf.Variable(tf.zeros([1]), name="d_b3", dtype=tf.float32)
-    h3 = tf.matmul(h2, w3) + b3
-    y_data = tf.nn.sigmoid(tf.slice(h3, [0, 0], [batch_size, -1], name=None))
-    y_generated = tf.nn.sigmoid(tf.slice(h3, [batch_size, 0], [-1, -1], name=None))
-    d_params = [w1, b1, w2, b2, w3, b3]
-    return y_data, y_generated, d_params
+    with tf.variable_scope('discriminator'):
+        x_in = tf.concat([x_data, x_generated], 0)
+
+        h1 = tf.layers.dense(x_in, h2_size, tf.nn.relu)
+        h1 = tf.nn.dropout(h1, keep_prob)
+
+        h2 = tf.layers.dense(h1, h1_size, tf.nn.relu)
+        h2 = tf.nn.dropout(h2, keep_prob)
+
+        h3 = tf.layers.dense(h2, 1, tf.nn.sigmoid)  # (batch_size*2, 1)
+        h3 = tf.reshape(h3, [-1])  # (batch_size*2, )
+
+        y_data = tf.slice(h3, [0], [batch_size])  # (batch_size, )
+        y_generated = tf.slice(h3, [batch_size], [-1])  # # (batch_size, )
+    return y_data, y_generated
 
 
 def show_result(batch_res, fname, grid_size=(8, 8), grid_pad=5):
@@ -86,38 +77,41 @@ def show_result(batch_res, fname, grid_size=(8, 8), grid_pad=5):
 def train():
     mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 
+    # placeholder
     x_data = tf.placeholder(tf.float32, [batch_size, img_size], name="x_data")
     z_prior = tf.placeholder(tf.float32, [batch_size, z_size], name="z_prior")
     keep_prob = tf.placeholder(tf.float32, name="keep_prob")
     global_step = tf.Variable(0, name="global_step", trainable=False)
 
-    x_generated, g_params = build_generator(z_prior) # x_generated:(batch_size, 784)
-    y_data, y_generated, d_params = build_discriminator(x_data, x_generated, keep_prob) # y:(batch_size, 1)
+    # G and D
+    x_generated = build_generator(z_prior)  # x_generated:(batch_size, 784)
+    y_data, y_generated = build_discriminator(x_data, x_generated, keep_prob)  # y:(batch_size, 1)
 
     d_loss = - tf.reduce_mean(tf.log(y_data) + tf.log(1 - y_generated))
     g_loss = - tf.reduce_mean(tf.log(y_generated))
 
-    ''' 
-    with tf.Session() as sess:
-        sess.run(tf.global_variables_initializer())
-        print(g_loss, d_loss)
-        assert False
-    '''
     optimizer = tf.train.AdamOptimizer(0.0001)
+
+    d_params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='discriminator')
+    g_params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='generator')
 
     d_trainer = optimizer.minimize(d_loss, var_list=d_params)
     g_trainer = optimizer.minimize(g_loss, var_list=g_params)
-
-    init = tf.global_variables_initializer()
 
     saver = tf.train.Saver()
 
     sess = tf.Session()
 
-    sess.run(init)
-    
+    sess.run(tf.global_variables_initializer())
+
+    # summary
+    tf.summary.scalar('g_loss', g_loss)
+    tf.summary.scalar('d_loss', d_loss)
+    summary_all = tf.summary.merge_all()
+    train_writer = tf.summary.FileWriter('logs', sess.graph)
+
     if to_restore:
-        chkpt_fname = tf.train.latest_checkpoint(output_path) # remove file recursively
+        chkpt_fname = tf.train.latest_checkpoint(output_path)  # remove file recursively
         saver.restore(sess, chkpt_fname)
     else:
         if os.path.exists(output_path):
@@ -132,14 +126,18 @@ def train():
             x_value, _ = mnist.train.next_batch(batch_size)
             x_value = 2 * x_value.astype(np.float32) - 1
             z_value = np.random.normal(0, 1, size=(batch_size, z_size)).astype(np.float32)
-            sess.run(d_trainer, feed_dict={x_data: x_value, z_prior: z_value, keep_prob: 0.7})
-            if j % 1 == 0:
-                sess.run(g_trainer, feed_dict={x_data: x_value, z_prior: z_value, keep_prob: 0.7})
+            sess.run([d_trainer], feed_dict={x_data: x_value, z_prior: z_value, keep_prob: 0.7})  # train D
+            if j % 1 == 0:  # %1是什么鬼
+                sess.run([g_trainer], feed_dict={x_data: x_value, z_prior: z_value, keep_prob: 0.7})  # train G
+
+            train_summary = sess.run(summary_all, feed_dict={x_data: x_value, z_prior: z_value, keep_prob: 0.7})
+            train_writer.add_summary(train_summary, i*int(60000 / batch_size)+j)
+
         x_gen_val = sess.run(x_generated, feed_dict={z_prior: z_sample_val})
         show_result(x_gen_val, os.path.join(output_path, "sample%s.jpg" % i))
-        #z_random_sample_val = np.random.normal(0, 1, size=(batch_size, z_size)).astype(np.float32)
-        #x_gen_val = sess.run(x_generated, feed_dict={z_prior: z_random_sample_val})
-        #show_result(x_gen_val, os.path.join(output_path, "random_sample%s.jpg" % i))
+        # z_random_sample_val = np.random.normal(0, 1, size=(batch_size, z_size)).astype(np.float32)
+        # x_gen_val = sess.run(x_generated, feed_dict={z_prior: z_random_sample_val})
+        # show_result(x_gen_val, os.path.join(output_path, "random_sample%s.jpg" % i))
         sess.run(tf.assign(global_step, i + 1))
         saver.save(sess, os.path.join(output_path, "model"), global_step=global_step)
 
